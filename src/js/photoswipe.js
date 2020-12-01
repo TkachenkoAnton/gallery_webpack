9823b4b592393481131603a040411376cfae240e
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-12-02 00:08:22 +0200

    добавляет разметку

---
+++ src/js/photoswipe.js
@@ -0,0 +1,39 @@
+// import PhotoSwipe from 'photoswipe';    
+
+// const openPhotoSwipe = function () {
+//         const pswpElement = document.querySelectorAll('.pswp')[0];
+
+//         // build items array
+//         const items = [
+//             {
+//                 src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
+//                 w: 964,
+//                 h: 1024
+//             },
+//             {
+//                 src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
+//                 w: 1024,
+//                 h: 683
+//             }
+//         ];
+        
+//         // define options (if needed)
+//         const options = {
+//            // history & focus options are disabled on CodePen        
+//             history: false,
+//             focus: false,
+
+//             showAnimationDuration: 0,
+//             hideAnimationDuration: 0
+            
+//         };
+        
+//         const gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+//         gallery.init();
+//     };
+
+//     openPhotoSwipe();
+
+//     document.getElementById('btn').onclick = openPhotoSwipe;
+
+
